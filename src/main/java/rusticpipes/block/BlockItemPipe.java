a621b8abe40864580e5c6b4259fd6f2ded9aa3e5
Vincent <nlblackeagle@live.nl>
2026-05-01 05:19:55 +0200

    Wrote BlockItemPipe (Pipe base class)

---
+++ src/main/java/rusticpipes/block/BlockItemPipe.java
@@ -0,0 +1,147 @@
+package rusticpipes.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import rusticpipes.tileentity.TileEntityItemPipe;
+import rusticpipes.network.PipeNetwork;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BlockItemPipe extends Block implements ITileEntityProvider {
+
+    public static final PropertyDirection FACING = PropertyDirection.create("facing");
+    public static final PropertyBool UP = PropertyBool.create("up");
+    public static final PropertyBool DOWN = PropertyBool.create("down");
+    public static final PropertyBool NORTH = PropertyBool.create("north");
+    public static final PropertyBool EAST = PropertyBool.create("east");
+    public static final PropertyBool SOUTH = PropertyBool.create("south");
+    public static final PropertyBool WEST = PropertyBool.create("west");
+
+    private static final Map<EnumFacing, PropertyBool> FACE_TO_PROPERTY = new HashMap<>();
+
+    static {
+        FACE_TO_PROPERTY.put(EnumFacing.NORTH, NORTH);
+        FACE_TO_PROPERTY.put(EnumFacing.SOUTH, SOUTH);
+        FACE_TO_PROPERTY.put(EnumFacing.EAST,  EAST);
+        FACE_TO_PROPERTY.put(EnumFacing.WEST,  WEST);
+        FACE_TO_PROPERTY.put(EnumFacing.UP,    UP);
+        FACE_TO_PROPERTY.put(EnumFacing.DOWN,  DOWN);
+    }
+
+    public BlockItemPipe() {
+        super(Material.IRON);
+        setDefaultState(blockState.getBaseState()
+                .withProperty(FACING, EnumFacing.NORTH)
+                .withProperty(UP, false)
+                .withProperty(DOWN, false)
+                .withProperty(NORTH, false)
+                .withProperty(EAST, false)
+                .withProperty(SOUTH, false)
+                .withProperty(WEST, false));
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean hasTileEntity(IBlockState state) {
+        return true;
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(World world, int meta) {
+        return new TileEntityItemPipe();
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, FACING,UP,DOWN,NORTH,EAST,SOUTH,WEST);
+    }
+
+    @Override
+    public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing,
+                                            float hitX, float hitY, float hitZ,
+                                            int meta, EntityLivingBase placer, EnumHand hand)
+    {
+        return getDefaultState()
+                .withProperty(FACING, EnumFacing.getDirectionFromEntityLiving(pos, placer))
+                .withProperty(UP, false)
+                .withProperty(DOWN, false)
+                .withProperty(NORTH, false)
+                .withProperty(EAST, false)
+                .withProperty(SOUTH, false)
+                .withProperty(WEST, false);
+    }
+
+    @Override
+    public void neighborChanged(IBlockState state, World world, BlockPos pos,
+                                Block blockIn, BlockPos fromPos) {
+
+        TileEntity te = world.getTileEntity(pos);
+        if (te instanceof TileEntityItemPipe) {
+            TileEntityItemPipe pipe = (TileEntityItemPipe) te;
+            pipe.refreshConnections();
+        }
+
+    }
+
+    @Override
+    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state,
+                                    EntityPlayer player, EnumHand hand,
+                                    EnumFacing facing, float hitX, float hitY, float hitZ) {
+
+        if (!player.isSneaking()) return false;
+
+        if (world.isRemote) return true;
+
+        PipeNetwork network = PipeNetwork.getNetwork(world, pos);
+        if (network == null) return false;
+        network.reverseNetwork(world);
+
+        return true;
+
+    }
+
+    @Override
+    public void breakBlock(World world, BlockPos pos, IBlockState state) {
+        TileEntity te = world.getTileEntity(pos);
+        if (te instanceof TileEntityItemPipe) {
+            TileEntityItemPipe pipe = (TileEntityItemPipe) te;
+            pipe.onRemoved();
+        }
+        super.breakBlock(world, pos, state);
+    }
+
+    @Override
+    public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos pos) {
+        TileEntity te = world.getTileEntity(pos);
+        if (!(te instanceof TileEntityItemPipe)) return state;
+        TileEntityItemPipe pipe = (TileEntityItemPipe) te;
+
+        for (EnumFacing face : EnumFacing.VALUES) {
+            state = state.withProperty(FACE_TO_PROPERTY.get(face), pipe.isConnected(face));
+        }
+        return state;
+    }
+}
